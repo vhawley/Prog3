cd24aaa686ee0aed3c1c550f4c7e5aba9e916901
Victor Hawley Jr <victor@victorhawley.com>
2015-10-10 21:31:28 -0400

    Added file reading
    Server now checks for the requested file and returns its contents if it
    is readable

--- tcpserver.c
+++ tcpserver.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
     }
     else {
         port = atoi(argv[1]);
-        if (port < 1) {
+        if (port < 1 || port > 65536) {
             fprintf(stderr, "error: port must be positive integer\n");
             exit(1);
         }
@@ -52,24 +52,28 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     
+    //bind socket
     if (bind(s, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
         fprintf(stderr, "error binding tcp socket\n");
         exit(1);
     }
     
+    //listen on tcp port
     if (listen(s, MAX_PENDING) < 0) {
         fprintf(stderr, "error listening on tcp socket\n");
         exit(1);
     }
     
-    printf("Welcome to TCP SERVER\n");
-    
+    //server loop
     while (1) {
+        //accept message
         new_s = accept(s, (struct sockaddr *)&sin, &len);
         if (new_s < 0) {
             fprintf(stderr, "error accepting message from client\n");
             exit(1);
         }
+        
+        //receive message
         len = recv(new_s, buf, sizeof(buf), 0);
         printf("%d\n", len);
         if (len == -1) {
@@ -80,9 +84,39 @@ int main(int argc, char *argv[]) {
             break;
         }
         printf("TCP Server Received: %s\n", buf);
+        
+        FILE *f;
+        char *message = malloc(sizeof(char));
+        message[0] = 0;
+        int fileSize;
+        
+        //attempt to read file.
+        f = fopen(buf, "r");
+        if (f == NULL)
+        {
+            fileSize = -1;
+        }
+        else {
+            fseek(f, 0L, SEEK_END);
+            fileSize = ftell(f); // get size of file
+            fseek(f, 0L, SEEK_SET);
+            message = malloc(sizeof(char)*fileSize);
+            int result = fread(message, 1, fileSize, f);
+            printf("%d %d\n", fileSize, result);
+        }
+        
+        //send file contents back to client if it exists, empty message with length -1 otherwise
+        printf("%s %d\n", message, fileSize);
+        if (send(new_s, message, fileSize, 0) < 0) {
+            fprintf(stderr, "error sending message back to client\n");
+            exit(1);
+        }
+        
+        //clean stuff
         close(new_s);
         fflush(stdout);
         bzero(buf, sizeof(buf));
+        bzero(message, sizeof(message));
     }
     
     return 0;
